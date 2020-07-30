7306b39afb1dde155d3e0b4224929fd1aac47b83
Tony Green <tgreen@albatrossflavour.com>
2020-07-30 16:14:01 +1000

    syntax fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,7 +44,7 @@ class pe_nc_backup (
     ensure => file,
     path   => "${bin_dir}/pe_nc_backup",
     mode   => '0755',
-    source => epp("${module_name}/pe_nc_backup.epp", {
+    source => epp('pe_nc_backup/pe_nc_backup.epp', {
       ssl_dir => $ssl_dir,
     })
   }
